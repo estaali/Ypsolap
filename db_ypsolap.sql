d33f2b6c81d11c2d23353e6e93517ccb81df7e6f
Emir STAALI <estaali@iform.fr>
2024-05-02 01:02:21 +0200

    Mise à jour importante du fichier source SQL et corrections PHP.

--- db_ypsolap.sql
+++ db_ypsolap.sql
@@ -1,77 +1,44 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1deb3
--- https://www.phpmyadmin.net/
+-- MariaDB dump 10.19  Distrib 10.11.7-MariaDB, for debian-linux-gnu (x86_64)
+-- Admin: Emir STAALI
+-- Project: Projet Fil Rouge No 2
+-- Host: localhost    Database: db_ypsolap
+-- ------------------------------------------------------
+-- Server version	10.11.7-MariaDB-2ubuntu2
 --
--- Hôte : localhost:3306
--- Généré le : mer. 01 mai 2024 à 13:32
--- Version du serveur : 10.11.7-MariaDB-2ubuntu2
--- Version de PHP : 8.3.6
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
+-- Current Database: `db_ypsolap`
 --
--- Base de données : `db_ypsolap`
---
-CREATE DATABASE IF NOT EXISTS `db_ypsolap` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `db_ypsolap` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
+
 USE `db_ypsolap`;
 
--- --------------------------------------------------------
+--
+-- Table structure for table `tbl_users`
+--
 
---
--- Structure de la table `tbl_users`
---
+DROP TABLE IF EXISTS `tbl_users`;
 
 CREATE TABLE `tbl_users` (
-  `id` int(64) NOT NULL,
+  `id` int(64) NOT NULL AUTO_INCREMENT,
   `nom` varchar(256) NOT NULL,
   `prenom` varchar(256) NOT NULL,
-  `promo` varchar(256) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `promo` varchar(256) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Déchargement des données de la table `tbl_users`
+-- Dumping data for table `tbl_users`
 --
 
-INSERT INTO `tbl_users` (`id`, `nom`, `prenom`, `promo`) VALUES
-(1, 'Doe', 'John', '2024'),
-(3, 'Dupond', 'Jean', '2024'),
-(4, 'Linux', 'Debian', '2024'),
-(5, 'Darc', 'Jeanne', '2024'),
-(6, 'Lungo', 'Cafe', '2024'),
-(7, 'Villa', 'Pancho', '2024'),
-(8, 'Aston', 'Martin', '2024'),
-(9, 'Mere', 'Michelle', '2024'),
-(10, 'Andre', 'Alex', '2024');
+INSERT INTO `tbl_users` VALUES
+(1,'Doe','John','2024'),
+(3,'Dupond','Jean','2024'),
+(4,'Linux','Debian','2024'),
+(5,'Darc','Jeanne','2024'),
+(6,'Lungo','Cafe','2024'),
+(7,'Villa','Pancho','2024'),
+(8,'Aston','Martin','2024'),
+(9,'Mere','Michelle','2024'),
+(10,'Andre','Alex','2024');
 
---
--- Index pour les tables déchargées
---
-
---
--- Index pour la table `tbl_users`
---
-ALTER TABLE `tbl_users`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT pour les tables déchargées
---
-
---
--- AUTO_INCREMENT pour la table `tbl_users`
---
-ALTER TABLE `tbl_users`
-  MODIFY `id` int(64) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- Dump completed on 2024-05-01 18:21:47
